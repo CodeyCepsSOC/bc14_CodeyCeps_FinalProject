1d8b99d73c543165faa33d8860709007a2e03e9c
samisile <samisile97@gmail.com>
2023-06-08 15:05:17 +0100

    testing deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. I Like to eat cheese. 
+          Edit <code>src/App.js</code> and save to reload. I Like to eat cheese and beans.  
         </p>
         <a
           className="App-link"
