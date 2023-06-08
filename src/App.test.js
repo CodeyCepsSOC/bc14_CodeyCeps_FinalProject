e5644f206ae0f951a3ea96d8c903362c17d929b1
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-06-08 15:31:46 +0100

    its gonna work now

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import {test, expect} from '@testing-library/jest-dom'
 import App from './App';
 
 test('renders learn react link', () => {
