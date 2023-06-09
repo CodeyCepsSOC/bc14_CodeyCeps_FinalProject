f94ea324ee9d7e972721b774a5106c2d39482abb
samisile <samisile97@gmail.com>
2023-06-09 11:16:15 +0100

    this will work

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/WalkMate/i);
+  const linkElement = screen.getByText(/WalkMate coming soon!/i);
   expect(linkElement).toBeInTheDocument();
 });
 
