86b11613d44bd51b38bd11c7024e6ea899c16627
HildaSig <hilda-sig@hotmail.com>
2023-06-12 12:19:59 +0100

    Route Dreail card function written

--- src/ExplorePage/RouteDetailCard/index.js
+++ src/ExplorePage/RouteDetailCard/index.js
@@ -1,1 +1,24 @@
 //card on side of map to display info about route
+
+import { useEffect } from "react"
+
+export default function RouteDetailCard (props) {
+
+
+    useEffect( () => { 
+
+    }, [])
+
+return (
+<div className="route-detail-card">
+    <h2>{props.name}</h2>
+    <section className="icons">
+        <div className="route-time">
+            <img src="https://img.icons8.com/ios/50/000000/clock--v1.png" alt="clock icon" />
+            <p>{props.time}</p>
+        </div>
+        {props.icons.map( (icon, index) => {return <img key={index} src={`../../../public/${icon}.png`} alt="icon" />})}
+    </section>
+    <p>{props.description}</p>
+</div>)
+}
--- src/ExplorePage/index.js
+++ src/ExplorePage/index.js
@@ -1,1 +1,15 @@
 // page file for the route detail dropdown
+
+import MapDisplay from './MapDisplay';
+import RouteDetailCard from './RouteDetailCard';
+
+export default function ExplorePage() {
+
+
+    return (
+        <div className="explore-page">
+            <RouteDetailCard />
+            <MapDisplay />
+        </div>
+    )
+}
