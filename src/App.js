d366474fcad3a9c8857c0404fe1195e242ae52af
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-06-12 11:50:47 +0100

    map displaying on page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-
+import MapDisplay from './ExplorePage/MapDisplay';
 function App() {
   return (
     <div className="App">
       {<h1>
         WalkMate Coming Soon!
       </h1> }
-
+        <MapDisplay/>
     </div>
   );
 }
--- src/ExplorePage/MapDisplay/index.js
+++ src/ExplorePage/MapDisplay/index.js
@@ -1,1 +1,31 @@
 // map display from API
+import React, { useRef, useEffect, useState } from 'react';
+import './mapdisplay.css'
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY
+
+export default function MapDisplay(props) {
+
+const mapContainer = useRef(null)
+const map = useRef(null)
+
+useEffect(()=> {
+    if (map.current) return; // initialize map only once
+
+    // Initialize map
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/outdoors-v12',
+      center: [-0.087305, 50.861966],
+      zoom: 11,
+    });
+
+}, [])
+
+
+    return (
+        <div ref={mapContainer} className="map-container"/>
+    )
+}
