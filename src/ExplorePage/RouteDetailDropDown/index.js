18bc5df631b54d39c7b3b2f84b62fd57c841d644
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-06-13 16:25:36 +0100

    dropdown css done

--- src/ExplorePage/RouteDetailDropDown/index.js
+++ src/ExplorePage/RouteDetailDropDown/index.js
@@ -5,20 +5,20 @@ import details bar functionality fom details bar js file,
 static image of route - with route map
 p tag for brief description
 */
-
+import './routedetailsdropdown.css'
 import react from 'react';
 import DetailsBar from './DetailsBar';
 
 function RouteDetailDropdown(props) {
 return (
-    <div>
+    <div className="route-details-dropdown">
         <h1>{props.name}</h1>
         <div className="carousel">
         </div>
         <DetailsBar {...props}/>
         <section className="dropdown-body">            
             <p>{props.description}</p>
-            <img src={props.map} alt="route map"/>
+            <img src={props.map} alt="route map" id="map-image"/>
         </section>
     </div>
 ) 
