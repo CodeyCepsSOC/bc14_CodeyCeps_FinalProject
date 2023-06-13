0339fbae70cb909e33db682a7991a46cb3ba8a90
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-06-13 12:21:30 +0100

    fonts added and working

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 import App from './App';
+import './fonts/PPWoodland-Ultralight.ttf';
+import './fonts/PPWoodland-Bold.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
