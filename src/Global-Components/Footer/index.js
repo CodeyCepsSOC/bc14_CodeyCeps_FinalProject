7e32327d171333cdfdf51c5a3d3735b79f64de50
Jordan Rutnam <jordan.rutnam@gmail.com>
2023-06-15 14:06:06 +0100

    Footer page skeleton finished

--- src/Global-Components/Footer/index.js
+++ src/Global-Components/Footer/index.js
@@ -2,28 +2,32 @@ import react from 'react';
 
 export default function Footer() {
 return(
-<div> 
-<button>App logo
-</button>
-<div>
+<div className="logo"> 
+<h1>WalkMate</h1>
+    <img src="href" alt="logo"/>
+<div className="home">
     <ul>
+    <img src="href" alt="home"/>
         <li>Home</li>
         <li>About us</li>
         <li>events</li>
     </ul>
 </div>
-<div>
+<div className="explore">
     <ul>
+    <img src="href" alt="explore"/>
         <li>Explore</li>
     </ul>
 </div>
-<div>
+<div className="community">
         <ul>
+        <img src="href" alt="community"/>
             <li>Community</li>
             </ul>
 </div>
-<div>
+<div className="signup">
         <ul>
+        <img src="href" alt="signup"/>
         <li>Sign up</li>
         </ul>
 </div>
