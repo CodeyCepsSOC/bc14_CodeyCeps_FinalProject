be8df722d752b50b93599b93b16a65647a68e619
HildaSig <hilda-sig@hotmail.com>
2023-06-15 10:59:39 +0100

    Added sample routes and got them to display on map, needd to work on being able to change routes

--- src/pages/ExplorePage/MapDisplay/index.js
+++ src/pages/ExplorePage/MapDisplay/index.js
@@ -2,10 +2,12 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './mapdisplay.css'
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import { matchPath } from 'react-router-dom';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY
 
 export default function MapDisplay(props) {
+
 
 const mapContainer = useRef(null)
 const map = useRef(null)
@@ -22,7 +24,33 @@ useEffect(()=> {
       zoom: 11,
     });
 
-}, [])
+    map.current.on('load', function () {
+        map.current.addSource('route', {
+            'type': 'geojson',
+            'data': {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+            'type': 'LineString',
+            'coordinates': props.routeDetailArray[props.activeRoute].coordinateArray
+            }
+            }
+            });
+            map.current.addLayer({
+            'id': 'route',
+            'type': 'line',
+            'source': 'route',
+            'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+            },
+            'paint': {
+            'line-color': '#354C33',
+            'line-width': 8
+            }
+            });
+            }
+)}, [props.activeRoute, props.routeDetailArray])
 
 
     return (
@@ -33,4 +61,18 @@ useEffect(()=> {
 /* this components renders the map onto the page, line 6 assigns the api key stored in the .env 
 file to the access token of the mapboxgl package. The use of useRef allows us to change the map container without causing a complete
 rerender of the page. We assign a new mapboxgl.map object to the map ref, which is then appended to the div below.
+
+
+| Displaying routes on map plan |
+
+- To create a geoJSON source (which is like a element in figma, it is an array of an array of cordinates)
+    - Find routes online and download them as a gpx file 
+    - Convert the gpx file to a geoJSON file using a converter online
+
+- Then we want to place that source as a layer on top of the map
+    - Mapbox has a .addlayer function, the type will be a line 
+    - We can then style the line using the paint property
+    - We can also add a filter to the layer to only show the route when it is selected
+
+- We could add an on hover or on click effect to the layer
 */
--- src/pages/ExplorePage/index.js
+++ src/pages/ExplorePage/index.js
@@ -7,7 +7,6 @@ import Search from './Search';
 import RouteDetailDropdown from './RouteDetailDropDown';
 
 export default function ExplorePage() {
- // defining route detail card props in an object to make it easier to pass to the component. Hard coded for now.
 
  const [activeRoute, setActiveRoute] = useState(0)
 
@@ -20,7 +19,40 @@ export default function ExplorePage() {
             map: './walk1.jpeg',
             img: './walk1.jpeg',
             id: 0,
-            distance: 5
+            distance: 5,
+            coordinateArray: [  [-0.087305, 50.861966],
+            [-0.087767, 50.862192],
+            [-0.088406, 50.863388],
+            [-0.090415, 50.86353],
+            [-0.09382, 50.86191],
+            [-0.094887, 50.863522],
+            [-0.097866, 50.865077],
+            [-0.101194, 50.866847],
+            [-0.101251, 50.869075],
+            [-0.102085, 50.870559],
+            [-0.102396, 50.873806],
+            [-0.100748, 50.881361],
+            [-0.100148, 50.884316],
+            [-0.095066, 50.887123],
+            [-0.09071, 50.891505],
+            [-0.097029, 50.898539],
+            [-0.10414, 50.900723],
+            [-0.106738, 50.901782],
+            [-0.105025, 50.900991],
+            [-0.09976, 50.899455],
+            [-0.091236, 50.899715],
+            [-0.078377, 50.898458],
+            [-0.069848, 50.897245],
+            [-0.072141, 50.881828],
+            [-0.077448, 50.877548],
+            [-0.081315, 50.869934],
+            [-0.076902, 50.865718],
+            [-0.081207, 50.864111],
+            [-0.08328, 50.863654],
+            [-0.08615, 50.863442],
+            [-0.088025, 50.86223],
+            [-0.087305, 50.861966],
+            ]
          },
          {
             name: "Isaacs Walk",
@@ -30,7 +62,39 @@ export default function ExplorePage() {
             map: './walk2.jpeg',
             img: './walk2.jpeg',
             id:1,
-            distance: 10
+            distance: 10,
+            coordinateArray: [[-0.07111, 50.840358],
+            [-0.075121, 50.840251],
+            [-0.064682, 50.847759],
+            [-0.053361, 50.850427],
+            [-0.042812, 50.854196],
+            [-0.039294, 50.855327],
+            [-0.035777, 50.855919],
+            [-0.029601, 50.858949],
+            [-0.024713, 50.85624],
+            [-0.022427, 50.856823],
+            [-0.023965, 50.856087],
+            [-0.028397, 50.857842],
+            [-0.034037, 50.856623],
+            [-0.035392, 50.855004],
+            [-0.034141, 50.852739],
+            [-0.031348, 50.850585],
+            [-0.034275, 50.84951],
+            [-0.036711, 50.846782],
+            [-0.039016, 50.844792],
+            [-0.038035, 50.842752],
+            [-0.043029, 50.838316],
+            [-0.045507, 50.835336],
+            [-0.050338, 50.83342],
+            [-0.053225, 50.834549],
+            [-0.052652, 50.83566],
+            [-0.047568, 50.83785],
+            [-0.051146, 50.840347],
+            [-0.055415, 50.841772],
+            [-0.065335, 50.840973],
+            [-0.06985, 50.841218],
+            [-0.075121, 50.840251],
+            [-0.07111, 50.840358]]
          },
          {
             name: "Samis Walk",
@@ -40,7 +104,8 @@ export default function ExplorePage() {
             map: './walk3.jpeg',
             img: './walk3.jpeg',
             id: 2,
-            distance: 21
+            distance: 21,
+            coordinateArray: []
          }
     ]
     
@@ -55,10 +120,9 @@ export default function ExplorePage() {
                     <Search/>
                     <div className="route-cards">
                     {routeDetailArray.map((route)=> <RouteDetailCard onClick={onClickSetRoute} key={route.id} {...route}/>)}
-
                     </div>
                 </div>
-                <MapDisplay />
+                <MapDisplay routeDetailArray={routeDetailArray} activeRoute={activeRoute}/>
             </div>
             <RouteDetailDropdown {...routeDetailArray[activeRoute]}/>
         </div>
