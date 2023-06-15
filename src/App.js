d0dfdc90f589d5f2dcd740b4ed60fd40e7b0bfa8
samisile <samisile97@gmail.com>
2023-06-15 12:19:43 +0100

    added css styling for navbar aka header

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import ExplorePage from './ExplorePage';
 import MapDisplay from './ExplorePage/MapDisplay';
 import CarouselTest from './Global-Components/Carousel';
 import LandingPage from './LandingPage';
+import Header from './Global-Components/Header';
 
 function App() {
   return (
     <div className="App">
-      {<h1>
+      {/* {<h1>
         WalkMate Coming Soon!
-      </h1> }
-
+      </h1> } */}
+        <Header />
         <ExplorePage/>
-
-      <LandingPage/>
-
+        <LandingPage/>
     </div>
   );
 }
