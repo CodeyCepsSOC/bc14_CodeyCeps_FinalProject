c25eed89f6c54388d8efe9fa68e4edcef8379107
Jordan Rutnam <jordan.rutnam@gmail.com>
2023-06-15 10:52:22 +0100

    footer branch created and skeletal structure of footer coded

---
+++ src/Global-Components/Footer/index.js
@@ -0,0 +1,32 @@
+import react from 'react';
+
+export default function Footer() {
+return(
+<div> 
+<button>App logo
+</button>
+<div>
+    <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>events</li>
+    </ul>
+</div>
+<div>
+    <ul>
+        <li>Explore</li>
+    </ul>
+</div>
+<div>
+        <ul>
+            <li>Community</li>
+            </ul>
+</div>
+<div>
+        <ul>
+        <li>Sign up</li>
+        </ul>
+</div>
+ </div>
+)
+};
