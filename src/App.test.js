ad5d036ce889173ed0dcdf166fc2ed90f7a62a00
safiyahl <safiyah.seedat@gmail.com>
2023-06-20 16:37:57 +0100

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
+// import App from './App';
+// import React from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/WalkMate coming soon!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/WalkMate coming soon!/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
 
