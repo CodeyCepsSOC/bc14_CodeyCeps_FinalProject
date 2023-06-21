724d7fcb00577d99023a0cb29ef0f6dbb6db5e65
Jordan Rutnam <jordan.rutnam@gmail.com>
2023-06-21 12:21:39 +0100

    profile card and css Saf changes

--- src/pages/CommunityPage/ProfileCard/index.js
+++ src/pages/CommunityPage/ProfileCard/index.js
@@ -1,15 +1,19 @@
-import './profilecard.css'
+import "./profilecard.css";
 
 export default function ProfileCard(props) {
-
-
-
-    return (
-        <div className="profile-card">
-            <div id="profile-header">
-            <img src={props.profileimage} id="profile-picture" alt={props.name + 'profile image'}/>
-            <h1>{props.name}</h1>
-            </div>      
-        </div>
-    )
+  return (
+    <div className="profile-card">
+      <div id="profile-header">
+        <img
+          src={props.profileimage}
+          id="profile-picture"
+          alt={props.name + "profile image"}
+        />
+        <h1>{props.name}</h1>
+        <p>{props.quote}</p>
+        <h2> {props.name}'s Interests </h2>
+        <img src="./hiking.png" alt="hiking" />
+      </div>
+    </div>
+  );
 }
--- src/pages/CommunityPage/index.js
+++ src/pages/CommunityPage/index.js
@@ -8,12 +8,12 @@ function handleClick(){
 
 export default function CommunityPage(props) {
 
-
-
     return (
     <div id="community-page">
         <h1>community page</h1>
-        <ProfileCard name="Sami" profileimage="./hilda.png"/>
+        <ProfileCard name="Sami" profileimage="./hilda.png" quote="WalkMate is a gamechanger! I love their regular events, they are so organised and they have the best routes" />
+        <ProfileCard name="Hilda" profileimage="./businessman.png" quote="WalkMate is a gamechanger! I love their regular events, they are so organised and they have the best routes" />
+        <ProfileCard name="Bryony" profileimage="./sami.png" quote="WalkMate is a gamechanger! I love their regular events, they are so organised and they have the best routes" />
         <div> {CommunityPageEvents.map((event, index) => <EventsCard {...event} 
         key={index}/>
         )}
