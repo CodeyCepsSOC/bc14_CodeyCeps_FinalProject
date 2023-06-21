9baa80e6938065af2b8444b4fc5e9d65482d636c
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-06-21 15:46:19 +0100

    profile card progress made

--- src/pages/CommunityPage/ProfileCard/index.js
+++ src/pages/CommunityPage/ProfileCard/index.js
@@ -2,18 +2,20 @@ import "./profilecard.css";
 
 export default function ProfileCard(props) {
   return (
-    <div className="profile-card">
-      <div id="profile-header">
-        <img
-          src={props.profileimage}
-          id="profile-picture"
-          alt={props.name + "profile image"}
-        />
+<div className="profile-card">
+    <div id="profile-header">
+        <img src={props.profileimage} id="profile-picture" alt={props.name + "profile image"} />
         <h1>{props.name}</h1>
-        <p>{props.quote}</p>
-        <h2> {props.name}'s Interests </h2>
-        <img src="./hiking.png" alt="hiking" />
-      </div>
     </div>
+    <i id="profile-quote">"{props.quote}"</i>
+    <div id="interests-section">
+        <h2> {props.name}'s Interests </h2>
+        <div id="interests-icons">
+            {props.interests.map((interest)=> {
+                return <img src={`/icons/${interest}.svg`} alt={interest} />}
+            )} 
+        </div>
+    </div> 
+</div>
   );
 }
