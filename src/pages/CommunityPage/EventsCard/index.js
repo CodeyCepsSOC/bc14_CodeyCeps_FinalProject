660dc5167e5b33f8a3647bed25ea6c0dc84ee897
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-06-22 10:53:47 +0100

    half of events card done

--- src/pages/CommunityPage/EventsCard/index.js
+++ src/pages/CommunityPage/EventsCard/index.js
@@ -8,9 +8,16 @@ export default function EventsCard(props) {
             <img className="event" src={img} alt="beautiful route"/>
             <div className="legend">
                     <h2>{title}</h2>
-                    <p>{description}</p>
-                    <p>{date}</p>
-                    <p>{time}</p>
+                    <div className="time-and-date">
+                        <div className="event-date">
+                        <img alt="calendar icon" src='/icons/calendar.svg' className="events-icon"/>
+                        <p>{date}</p>
+                        </div>
+                        <div className="event-time">
+                        <img alt="clock icon" src='/icons/black-clock.svg' className="events-icon"/>
+                        <p>{time}</p>
+                        </div>
+                    </div>
                     <p>{attendees}</p>
                     <button className="event-button">Join Event</button>
         </div>
