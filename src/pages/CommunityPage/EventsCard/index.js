4120dda97d3109e92bd007ee0898733830e34760
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-06-22 09:57:23 +0100

    some layout done

--- src/pages/CommunityPage/EventsCard/index.js
+++ src/pages/CommunityPage/EventsCard/index.js
@@ -1,16 +1,18 @@
+import './eventscard.css'
 export default function EventsCard(props) {
 
     const {img, title, description, date, time, attendees} = props
 
     return (
         <div className="events-card">
-      <img className="event" src={img} alt="beautiful route"/>
-                <div className="legend">
+            <img className="event" src={img} alt="beautiful route"/>
+            <div className="legend">
                     <h2>{title}</h2>
                     <p>{description}</p>
                     <p>{date}</p>
                     <p>{time}</p>
                     <p>{attendees}</p>
+                    <button className="event-button">Join Event</button>
         </div>
         </div>
     )
